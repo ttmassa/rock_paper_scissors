b72ace04dc856369256c19f66f5738f52fe3e99b
Massa Timothée <timothee.massa.perso@gmail.com>
2023-02-28 16:56:38 +0100

    End of first UI version. Game end once user or computer won 5 times

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@
 let choices = ["rock", "paper", "scissors"];
 let computerScore = 0;
 let playerScore = 0;
+let numberOfGames = 0;
 
 function getComputerChoice() {
     const i = Math.floor(Math.random() * choices.length);
@@ -9,53 +10,103 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection.toLowerCase();
+    if (playerScore == 5 || computerScore == 5) {
+        endGame();
+        return;
+    }
+
+    playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        console.log("Equality ! Try again !");
+        result.textContent = "Equality ! Try again !";
+        results.appendChild(result);
+        numberOfGames++;
     } else if ((playerSelection === choices[0] && computerSelection === choices[1]) || 
     (playerSelection === choices[1] && computerSelection === choices[2]) ||
     (playerSelection === choices[2] && computerSelection === choices[0])) {
         computerScore ++;
-        console.log("Computer Choose " + computerSelection + " ! Try again to beat it !");
+        score1.textContent = 'Computer: ' + computerScore;
+        computerScoreDisplay.appendChild(score1);
+        result.textContent = "Computer Choose " + computerSelection + " ! Try again to beat it !";
+        results.appendChild(result);
+        numberOfGames++;
     } else {
         playerScore++;
-        console.log("Nice one ! " + playerSelection + " beats " + computerSelection + " !");
+        score2.textContent = 'You..: ' + playerScore;
+        playerScoreDisplay.appendChild(score2);
+        result.textContent = "Nice one ! " + playerSelection + " beats " + computerSelection + " !";
+        results.appendChild(result);
+        numberOfGames++;
+    }
+
+    if (playerScore == 5 || computerScore == 5) {
+        endGame();
     }
 }
 
-function game() {
-        let playerSelection = prompt("What's your choice young man ?");
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-
+function endGame() {
     if (computerScore > playerScore) {
-        console.log("Computer are going to overtake humanity...");
+        result.textContent = "Computer are going to overtake humanity...";
+        results.appendChild(result);
     } else {
-        console.log("You just beat a computer, don't be too proud of yourself young man.");
+        result.textContent = "You just beat a computer, don't be too proud of yourself young man.";
+        results.appendChild(result);
     }
 }
 
 // UI section
-
-// Vise l'endroit où on les boutons apparaîtront dans l'HTML
 const container = document.querySelector('#container');
-container.style.cssText = 'display: flex; justify-content: space-around; align-items: center; min-height: 97.5vh;'; 
+const scores = document.querySelector('#scores');
+const results = document.querySelector('#results');
+const computerScoreDisplay = document.querySelector('#computerScore');
+const playerScoreDisplay = document.querySelector('#playerScore');
+
+const result = document.createElement('p');
+const score1 = document.createElement('p');
+const score2 = document.createElement('p');
+
+score1.textContent = 'Computer: ' + computerScore;
+computerScoreDisplay.appendChild(score1);
+
+score2.textContent = 'You..: ' + playerScore;
+playerScoreDisplay.appendChild(score2);
 
 // ROCK
 const rockButton = document.createElement("button");
+rockButton.classList.add('rockBtn');
 rockButton.innerText = 'ROCK';
 rockButton.style.cssText = 'padding: 50px';
 container.appendChild(rockButton);
 
 // PAPER
 const paperButton = document.createElement('button');
+paperButton.classList.add('paperBtn');
 paperButton.innerText = 'PAPER';
 paperButton.style.cssText = 'padding: 50px';
 container.appendChild(paperButton);
 
 // SCISSORS
 const scissorsButton = document.createElement('button');
+scissorsButton.classList.add('scissorsBtn');
 scissorsButton.innerText = 'SCISSORS';
 scissorsButton.style.cssText = 'padding: 50px';
 container.appendChild(scissorsButton);
+
+// Event listeners
+
+const rockBtn  = document.querySelector('.rockBtn');
+rockBtn.addEventListener('click', () => {
+    playRound("rock", getComputerChoice());
+});
+
+const paperBtn  = document.querySelector('.paperBtn');
+paperBtn.addEventListener('click', () => {
+    playRound("paper", getComputerChoice());
+});
+
+const scissorsBtn  = document.querySelector('.scissorsBtn');
+scissorsBtn.addEventListener('click', () => {
+    playRound("scissors", getComputerChoice());
+});
+
+game();
